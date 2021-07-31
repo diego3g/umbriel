6d92a40fb0f3ccb6e80feacc9c81265ae217b230
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-07-31 14:05:19 -0300

    refactor: Improve RequiredFields validation logic

--- src/infra/http/validation/RequiredFieldsValidator.ts
+++ src/infra/http/validation/RequiredFieldsValidator.ts
@@ -1,4 +1,4 @@
-import { Either, left, right } from '@core/logic/Either'
+import { Either, left, right } from '../../../core/logic/Either'
 import { MissingParamError } from './errors/MissingParamError'
 import { Validator } from './Validator'
 
@@ -6,13 +6,15 @@ export class RequiredFieldsValidator<T = any> implements Validator<T> {
   public validate(data: T): Either<MissingParamError, null> {
     const fields = Object.getOwnPropertyNames(data)
     for (const field of fields) {
-      if (data[field] !== false && !data[field]) {
+      if (
+        data[field] === null ||
+        data[field] === undefined ||
+        (typeof data[field] === 'string' && data[field].trim() === '')
+      ) {
         return left(new MissingParamError(field))
       }
     }
 
     return right(null)
-
-    // TODO: validate if a field is an object with "sub-properties", and if this object has required properties
   }
 }
