5320b074aa8c6aac6033d48211d750f22fa970ab
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-01 19:54:49 -0300

    chore: Allow user creation only by admins

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 
+import { adaptMiddleware } from '../adapters/ExpressMiddlewareAdapter'
 import { adaptRoute } from '../adapters/ExpressRouteAdapter'
 import { makeRegisterUserController } from '../factories/controllers/RegisterUserControllerFactory'
+import { makeEnsureAuthenticatedMiddleware } from '../factories/middlewares/EnsureAuthenticatedMiddlewareFactory'
 
 const usersRouter = express.Router()
 
-// usersRouter.use(adaptMiddleware(makeEnsureAuthenticatedMiddleware()))
+usersRouter.use(adaptMiddleware(makeEnsureAuthenticatedMiddleware()))
 
 usersRouter.post('/', adaptRoute(makeRegisterUserController()))
 
