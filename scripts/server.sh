3af636a3b3ec6cd369ff3022ebc2d3f9b00a1fb6
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-01 17:09:37 -0300

    fix: Add prisma generate to server startup

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 (cd /home/node/app && node_modules/.bin/prisma migrate deploy)
+(cd /home/node/app && node_modules/.bin/prisma generate)
 (cd /home/node/app && node dist/infra/http/server.js)
