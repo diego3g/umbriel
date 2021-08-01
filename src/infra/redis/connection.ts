41820251a0f2c49612f761ccb6d1bd039f1449eb
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-01 15:07:14 -0300

    chore: Allow to set a Redis password

--- src/infra/redis/connection.ts
+++ src/infra/redis/connection.ts
@@ -1,7 +1,13 @@
-import Redis from 'ioredis'
+import Redis, { RedisOptions } from 'ioredis'
 
-export const redisConnection = new Redis({
+const options: RedisOptions = {
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT ?? 6379),
   db: Number(process.env.REDIS_DB ?? 0),
-})
+}
+
+if (process.env.REDIS_PASS) {
+  options.password = process.env.REDIS_PASS
+}
+
+export const redisConnection = new Redis(options)
