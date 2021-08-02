6a62da3689822e2be75e8837fc66df38d5a63a38
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-01 22:57:10 -0300

    tests: Fix register user to authenticated users

--- src/modules/accounts/useCases/RegisterUser/RegisterUserController.spec.ts
+++ src/modules/accounts/useCases/RegisterUser/RegisterUserController.spec.ts
@@ -7,6 +7,11 @@ import request from 'supertest'
 import { app } from '@infra/http/app'
 import { prisma } from '@infra/prisma/client'
 import { redisConnection } from '@infra/redis/connection'
+import { createAndAuthenticateUser } from '@test/factories/UserFactory'
+
+const {
+  jwt: { token },
+} = createAndAuthenticateUser()
 
 describe('Register User (e2e)', () => {
   afterAll(async () => {
@@ -15,12 +20,15 @@ describe('Register User (e2e)', () => {
   })
 
   it('should be able to register new user', async () => {
-    const response = await request(app).post('/users').send({
-      name: 'John Doe',
-      email: 'john@doe.com',
-      password: '123456',
-      password_confirmation: '123456',
-    })
+    const response = await request(app)
+      .post('/users')
+      .set('x-access-token', token)
+      .send({
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password: '123456',
+        password_confirmation: '123456',
+      })
 
     expect(response.status).toBe(201)
 
@@ -32,11 +40,14 @@ describe('Register User (e2e)', () => {
   })
 
   it('should return an error if validation fails', async () => {
-    const response = await request(app).post('/users').send({
-      name: 'John Doe',
-      email: 'john@doe.com',
-      password: '123456', // missing password confirmation
-    })
+    const response = await request(app)
+      .post('/users')
+      .set('x-access-token', token)
+      .send({
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password: '123456', // missing password confirmation
+      })
 
     expect(response.status).toBe(400)
   })
