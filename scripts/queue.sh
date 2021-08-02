b5659078545f07cd0cfdd0d436c88c396a7db42a
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-02 11:33:35 -0300

    ci: Create kafka and queue startup scripts

---
+++ scripts/queue.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+(cd /home/node/app && node_modules/.bin/prisma migrate deploy)
+(cd /home/node/app && node_modules/.bin/prisma generate)
+(cd /home/node/app && node dist/infra/queue/worker.js)
