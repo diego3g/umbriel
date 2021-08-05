d579dde6d2e8dfe763bd4d0f24ebc155de2de8ca
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-08-05 10:05:13 -0300

    feat: setup validators in Controllers factories

--- src/infra/http/factories/controllers/AuthenticateUserControllerFactory.ts
+++ src/infra/http/factories/controllers/AuthenticateUserControllerFactory.ts
@@ -2,11 +2,14 @@ import { Controller } from '@core/infra/Controller'
 import { PrismaUsersRepository } from '@modules/accounts/repositories/prisma/PrismaUsersRepository'
 import { AuthenticateUser } from '@modules/accounts/useCases/AuthenticateUser/AuthenticateUser'
 import { AuthenticateUserController } from '@modules/accounts/useCases/AuthenticateUser/AuthenticateUserController'
+import { AuthenticateUserValidator } from '@modules/accounts/validation/AuthenticateUserValidator'
 
 export function makeAuthenticateUserController(): Controller {
   const prismaUsersRepository = new PrismaUsersRepository()
   const authenticateUser = new AuthenticateUser(prismaUsersRepository)
+  const validator = new AuthenticateUserValidator()
   const authenticateUserController = new AuthenticateUserController(
+    validator,
     authenticateUser
   )
 
