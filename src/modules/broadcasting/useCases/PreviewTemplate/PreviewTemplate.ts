38f857ccfe77281bbeefc1fd03b526519e9a6b3d
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-11 11:43:48 -0300

    fix: Remove default CSS from template preview HTML

--- src/modules/broadcasting/useCases/PreviewTemplate/PreviewTemplate.ts
+++ src/modules/broadcasting/useCases/PreviewTemplate/PreviewTemplate.ts
@@ -20,8 +20,8 @@ export class PreviewTemplate {
     }
 
     const previewContent = [
-      '<p style="margin-bottom:1em">Hey there,</p>',
-      '<p style="margin-bottom:1em">',
+      '<p>Hey there,</p>',
+      '<p>',
       'This is a sample of what content might look like in your course emails or',
       "broadcasts. You'll be able to use variables in the content of your",
       "messages to insert things like the subscriber's first name or email",
@@ -29,26 +29,26 @@ export class PreviewTemplate {
       'template.',
       '</p>',
       '<h1>This is an example heading</h1>',
-      '<p style="margin-bottom:1em">',
+      '<p>',
       'Donec sed odio dui. Morbi leo risus, porta ac consectetur ac,',
       'vestibulum at eros. Sed posuere consectetur est at lobortis. Morbi leo',
       'risus, porta ac consectetur ac, vestibulum at eros.',
       '<a href="https://rocketseat.com.br" rel="noreferrer noopener" target="_blank">And this is a link</a>',
       '</p>',
-      '<p style="margin-bottom:1em">',
+      '<p>',
       'Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit',
       'amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris',
       'condimentum nibh, ut fermentum massa justo sit amet risus. Donec',
       'ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur',
       'est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.',
       '</p>',
-      '<p style="margin-bottom:1em">',
+      '<p>',
       'Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit',
       'libero, a pharetra augue. Donec sed odio dui. Vivamus sagittis lacus',
       'vel augue laoreet rutrum faucibus dolor auctor. Vivamus sagittis lacus',
       'vel augue laoreet rutrum faucibus dolor auctor.',
       '</p>',
-      '<p style="margin-bottom:1em">- Your Name</p>',
+      '<p>- Your Name</p>',
     ]
 
     const content = contentOrError.value
