6f9a5e7c67c761dcdf7b6752b75466143b51fdee
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-11 12:32:57 -0300

    fix: Change GMail fake openings userAgent filter

--- src/modules/broadcasting/useCases/RegisterEvent/RegisterEventController.ts
+++ src/modules/broadcasting/useCases/RegisterEvent/RegisterEventController.ts
@@ -42,7 +42,14 @@ export class RegisterEventController implements Controller {
       const type = eventTypesMap[data.eventType]
       const meta = data[eventMetaMap[data.eventType]]
 
-      if (type === 'OPEN' && meta?.userAgent?.includes('GoogleImageProxy')) {
+      /**
+       * Filter false GMail openings
+       */
+      if (
+        type === 'OPEN' &&
+        meta?.userAgent ===
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246 Mozilla/5.0'
+      ) {
         return ok()
       }
 
