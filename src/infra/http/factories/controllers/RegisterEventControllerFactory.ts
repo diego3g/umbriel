4f23ea7ad77902d8f85338ffc601d5da6798cf6d
Diego Fernandes <diego.schell.f@gmail.com>
2021-08-11 15:06:09 -0300

    fix: Add contactsRepository to RegisterEvent factory

--- src/infra/http/factories/controllers/RegisterEventControllerFactory.ts
+++ src/infra/http/factories/controllers/RegisterEventControllerFactory.ts
@@ -2,10 +2,19 @@ import { Controller } from '@core/infra/Controller'
 import { PrismaRecipientsRepository } from '@modules/broadcasting/repositories/prisma/PrismaRecipientsRepository'
 import { RegisterEvent } from '@modules/broadcasting/useCases/RegisterEvent/RegisterEvent'
 import { RegisterEventController } from '@modules/broadcasting/useCases/RegisterEvent/RegisterEventController'
+import { PrismaContactsRepository } from '@modules/subscriptions/repositories/prisma/PrismaContactsRepository'
+import { PrismaSubscriptionsRepository } from '@modules/subscriptions/repositories/prisma/PrismaSubscriptionsRepository'
 
 export function makeRegisterEventController(): Controller {
   const prismaRecipientsRepository = new PrismaRecipientsRepository()
-  const registerEvent = new RegisterEvent(prismaRecipientsRepository)
+  const prismaSubscriptionsRepository = new PrismaSubscriptionsRepository()
+  const prismaContactsRepository = new PrismaContactsRepository(
+    prismaSubscriptionsRepository
+  )
+  const registerEvent = new RegisterEvent(
+    prismaRecipientsRepository,
+    prismaContactsRepository
+  )
   const registerEventController = new RegisterEventController(registerEvent)
 
   return registerEventController
